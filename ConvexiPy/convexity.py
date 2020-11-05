2d1251ba7991aa7f3b5a15b3cc11aa352a95889f
potatoboiler <1nova2hunter3@gmail.com>
2020-11-04 23:37:34 -0500

    Remove unused methods

--- ConvexiPy/convexity.py
+++ ConvexiPy/convexity.py
@@ -11,7 +11,6 @@ from scipy import spatial
 eng = matlab.engine.start_matlab()
 
 
-
 def load_image(infilename) -> np.ndarray:
     """
     load image into numpy array
@@ -46,22 +45,6 @@ def ptCount(p: np.ndarray) -> int:
     return area
 
 
-def area(p) -> float:
-    """
-    Takes in list of coordinates to spit out area, use with convex hull list
-    Green's theorem, as implemented here https://stackoverflow.com/questions/451426/how-do-i-calculate-the-area-of-a-2d-polygon
-    """
-    return 0.5 * abs(sum(x0*y1 - x1*y0 for ((x0, y0), (x1, y1)) in segments(p)))
-
-
-def segments(p):  #
-    """
-    Takes in list of coordinates, wraps around first coord to last
-    https://stackoverflow.com/questions/451426/how-do-i-calculate-the-area-of-a-2d-polygon
-    """
-    return zip(p, p[1:] + [p[0]])
-
-
 def convMATLAB(binimg: np.ndarray):
     """
     Calls MATLAB script to compute the convex hull and area
@@ -70,23 +53,3 @@ def convMATLAB(binimg: np.ndarray):
     return eng.computeconvex(matlab.uint16(np.ndarray.tolist(binimg)))
 
 
-def convexArea(binimg: np.ndarray) -> int:
-    """
-    DO NOT USE: Uses Python scipy and numpy arrays and functions to compute area of convex hull
-    """
-
-    convhull = spatial.ConvexHull(spatial.ConvexHull(np.array(arr2Pts(binimg))).points)
-    convarea = area(convhull.points)
-
-    """ # plots the convex hull 
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    for visible_facet in convhull.simplices[convhull.good]:
-        ax.plot(convhull.points[visible_facet, 0],
-                convhull.points[visible_facet, 1],
-                color='violet',
-                lw=6)
-    spatial.convex_hull_plot_2d(convhull, ax=ax)
-    plt.show()
-    """
-    return convarea
