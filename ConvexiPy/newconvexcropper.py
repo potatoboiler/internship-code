571897825a5ddf9c01716871734f3cc1f728c049
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2020-11-06 12:53:17 -0500

    Add BW scale callback and fix buttons

--- ConvexiPy/newconvexcropper.py
+++ ConvexiPy/newconvexcropper.py
@@ -217,7 +217,7 @@ class Cropper(tk.Tk, CropperMenuBar):
         self.bwToolsFrame = tk.LabelFrame(
             self._frame, text='Black/White tools', padx=0)
         self.bwThresholdScale = tk.Scale(
-            master=self.bwToolsFrame, from_=0, to=255, orient=tk.HORIZONTAL, label='Threshold')
+            master=self.bwToolsFrame, from_=0, to=255, orient=tk.HORIZONTAL, label='Threshold', command=self.updateBWT)
         self.bwThresholdScale.set(self.bw_thresh)
         self.bwInc = tk.Button(self.bwToolsFrame, text='+1', command=self.incBWT)
         self.bwDec = tk.Button(self.bwToolsFrame, text='-1', command=self.decBWT)
@@ -227,9 +227,9 @@ class Cropper(tk.Tk, CropperMenuBar):
         self.toolButtons.grid(row=0, column=1)
 
         # add these buttons back in later
-        #self.bwDec.grid(row=0, column=0)
+        self.bwDec.grid(row=0, column=0)
         self.bwThresholdScale.grid(row=0, column=1)
-        #self.bwInc.grid(row=0, column=2)
+        self.bwInc.grid(row=0, column=2)
 
         self.drawButton.grid(row=0, column=0)
         self.backButton.grid(row=0, column=1)
@@ -241,6 +241,10 @@ class Cropper(tk.Tk, CropperMenuBar):
     def decBWT(self):
         self.bw_thresh -= 1
         self.bwThresholdScale.set(self.bw_thresh)
+
+    def updateBWT(self, event):
+        self.bw_thresh = self.bwThresholdScale.get()
+        print(self.bw_thresh)
 
     def init_basic_menu(self):
         self.basicButtons = tk.LabelFrame(self._frame, text='Basics')
