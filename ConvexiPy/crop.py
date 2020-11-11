3cfcbed5f12a607b765b0335cea357df78a065e5
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2020-11-11 13:46:22 -0500

    Add output function and copy __main__ functionality

--- ConvexiPy/crop.py
+++ ConvexiPy/crop.py
@@ -93,11 +93,94 @@ class ConvexCropper(Cropper):
         # write convexity onto file
         ret, thresh = cv.threshold(
             cv.imread(filename, 0), 127, 255, cv.THRESH_BINARY)
-        
-        convexity = conv.ptCount(thresh) / conv.convMATLAB(thresh)[0] if not disableMATLABcomponents else 0
+
+        convexity = conv.ptCount(
+            thresh) / conv.convMATLAB(thresh)[0] if not disableMATLABcomponents else 0
 
         self.draw.text((croparea.left, croparea.top),
                        text=str(round(convexity, ndigits=4)),
-                       fill=(255,255,255),
+                       fill=(255, 255, 255),
                        font=font)
 
+    def output(self):
+        imgexts = ['jpg', 'tif', 'png', 'bmp']
+
+        # creates UI object from which file is selected and crops are perfroemd
+        
+        # directory where output images are stored
+        crops = os.listdir(self.newdir)
+
+        # get name of csv file to be generated
+        csvname = self.newdir + self.og_filename + '_conv.csv'
+
+        with open(csvname, 'w', newline='') as csvfile:
+            w2csv = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            w2csv.writerow(
+                ['filename', 'convexity', 'aggregate area', 'convex area'])
+
+            for file in crops:
+                if any(file.endswith(x) for x in imgexts):
+                    # read image from dir
+                    img = cv.imread(os.path.join(self.newdir, file), 0)
+
+                    # thresh = nd array of binary image, ret is not used
+                    ret, thresh = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+
+                    
+                    # write binarized image to disk
+                    f, e = os.path.splitext(str(file))
+                    filename = f + '_bin' + e
+                    filepath = os.path.join(self.newdir, filename)
+                    cv.imwrite(filepath, thresh)
+
+                    if not conv.disableMATLABcomponents:
+                        # write stats to csv
+                        agg_area = conv.ptCount(thresh)  # aggregate area
+                        print("projected area: ", agg_area)
+                        conv_area = conv.convMATLAB(thresh)  # convex hull area
+                        print("convex area: ", conv_area)
+                        w2csv.writerow(
+                            [filename, agg_area/conv_area, agg_area, conv_area])
+
+if __name__ == '__main__':
+    imgexts = ['jpg', 'tif', 'png', 'bmp']
+
+    # creates UI object from which file is selected and crops are perfroemd
+    root = ConvexCropper()
+    root.mainloop()
+
+    # directory where output images are stored
+    crops = os.listdir(root.newdir)
+
+    # get name of csv file to be generated
+    csvname = root.newdir + root.og_filename + '_conv.csv'
+
+    with open(csvname, 'w', newline='') as csvfile:
+        w2csv = csv.writer(csvfile, delimiter=',',
+                           quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        w2csv.writerow(
+            ['filename', 'convexity', 'aggregate area', 'convex area'])
+
+        for file in crops:
+            if any(file.endswith(x) for x in imgexts):
+                # read image from dir
+                img = cv.imread(os.path.join(root.newdir, file), 0)
+
+                # thresh = nd array of binary image, ret is not used
+                ret, thresh = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+
+                # write binarized image to disk
+                f, e = os.path.splitext(str(file))
+                filename = f + '_bin' + e
+                filepath = os.path.join(root.newdir, filename)
+                cv.imwrite(filepath, thresh)
+
+                if not conv.disableMATLABcomponents:
+                    # write stats to csv
+                    agg_area = conv.ptCount(thresh)  # aggregate area
+                    print("projected area: ", agg_area)
+                    conv_area = conv.convMATLAB(thresh)  # convex hull area
+                    print("convex area: ", conv_area)
+                    w2csv.writerow(
+                        [filename, agg_area/conv_area, agg_area, conv_area])
