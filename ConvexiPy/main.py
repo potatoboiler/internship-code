0472db20a6777b8736d13f64488a09ae0e828dc6
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2020-11-11 14:01:23 -0500

    Create linking main.py

---
+++ ConvexiPy/main.py
@@ -0,0 +1,28 @@
+import os
+from tkinter import filedialog as tkfd
+
+from PIL import Image
+
+from bw import BW
+from crop import ConvexCropper
+from retouch import Retouch
+
+file = tkfd.askopenfile(mode='rb', filetypes=[
+    ('Image Files', '.jpg .JPG .jpeg .JPEG .png .PNG .tif .TIF .tiff .TIFF'),
+    ('TIFF Image Files', '.tif .TIF .tiff .TIFF')
+])
+image = Image.open(file)
+filename = file.name
+extension = os.path.splitext(filename)[1]
+print(extension)
+
+root = BW(image=image, filename=filename)
+root.mainloop()
+
+image = Image.open('temp' + extension)
+root = Retouch(image=image, filename=filename)
+root.mainloop()
+
+image = Image.open('temp' + extension)
+root = ConvexCropper(image=image, filename=filename)
+root.mainloop()
